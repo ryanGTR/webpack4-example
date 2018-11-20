43cdb7bf38dfc123d66e20564de0b824e47f5a42
ryan <ryan@ryande-MacBook-Pro.local>
2018-11-20 16:55:51 +0800

    Add sass

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -44,6 +44,20 @@ module.exports = {
 				]
 			},
 			{
+				test: /\.sass$/,
+				use: [
+					{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader"
+					},
+					{
+						loader: "sass-loader"
+					}
+				]
+			},
+			{
 				test:/\.html$/,
 				use: [
 					
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 require("babel-runtime/regenerator");
 require("webpack-hot-middleware/client?reload=true");
-require("./main.css");
+require("./main.sass");
 require("./index.html");
