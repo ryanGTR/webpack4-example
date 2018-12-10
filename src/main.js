6115b03310d9a8d74fa198934229642690167bb0
ryan <ryan@ryande-MacBook-Pro.local>
2018-12-10 11:14:43 +0800

    fix problem

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 require('babel-runtime/regenerator');
 require('webpack-hot-middleware/client?reload=true');
 require('./main.sass');
 require('./index.html');
-=======
-require("babel-runtime/regenerator");
-require("webpack-hot-middleware/client?reload=true");
-require("./images/link.jpg")
-require("./main.sass");
-require("./index.html");
-require("./app");
->>>>>>> dd0a869732161c7ecb951734c98a59b6c32cb9b8
